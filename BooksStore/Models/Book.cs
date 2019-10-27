952b5a960b016d4c5f1ef1daa68b26dc997c5100
septemberEatMe <vitaliiseptember@gmail.com>
2019-10-27 14:18:35 +0200

    аadd Models and conString Fix er

---
+++ BooksStore/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksStore.Models
+{
+    public class Book
+    {
+        //Для EF требования 
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Autor { get; set; }
+
+        public int Price { get; set; }
+
+    }
+}
---
+++ BooksStore/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksStore.Models
+{
+    public class Purchase
+    {
+        //EF условность требует обозначения первичного ключа с постфиксом ID или иметь имя ID.
+        public int PurchaseID { get; set; }
+        //имя и фамилия покупателя
+        public string Person { get; set; }
+        public string Adress { get; set; }
+        //номер книги, которую купил.
+        public int BookId { get; set; }
+        //время покупки
+        public DateTime Date { get; set; }
+
+    }
+}
