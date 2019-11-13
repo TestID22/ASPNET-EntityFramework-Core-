dd23f6a0d9f8570fd2025e55b39c92876f9a12c7
septemberEatMe <vitaliiseptember@gmail.com>
2019-11-13 19:56:26 +0200

    Practice with Razor and Views

---
+++ RazorTest/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product product = new Product
+            {
+                ProductId = 1,
+                Name = "Ноутбук",
+                Description = "Hp with Kali Linux",
+                Price = 50_000.54m,
+                Category = "Computers"
+            };
+            ViewBag.StockLevel = 2;
+            return View(product);
+        }
+       
+    }
+}
