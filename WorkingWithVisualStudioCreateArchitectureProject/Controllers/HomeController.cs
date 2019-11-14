b618b8627e5536802787beedfc5ec905a022b3c6
septemberEatMe <vitaliiseptember@gmail.com>
2019-11-14 11:32:54 +0200

    create MVC add LibMan JQuery

---
+++ WorkingWithVisualStudioCreateArchitectureProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithVisualStudioCreateArchitectureProject.Models;
+
+namespace WorkingWithVisualStudioCreateArchitectureProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() 
+            => View(SimpleRepository.ShraredRepository.Products);
+        
+    }
+}
