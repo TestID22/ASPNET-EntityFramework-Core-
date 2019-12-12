aa4de4ef5ae7e34b1a5dbd9d2de792dba1c06d11
septemberEatMe <vitaliiseptember@gmail.com>
2019-12-12 15:57:22 +0200

    Start proj.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductSaver repo;
+
+        public ProductController(IProductSaver repo)
+        {
+            this.repo = repo;
+        }
+        public ViewResult List() => View(repo.Products);
+    }
+}
---
+++ SportsStore/Models/FakeProductStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductStorage : IProductSaver
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product{Name = "SkateBoard", Price = 100},
+            new Product{Name = "Snow", Price = 300}
+        };
+    }
+}
