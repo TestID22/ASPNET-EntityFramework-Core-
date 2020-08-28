a43854c2bbc16bd6e7cc9a7538645ab406176d2d
septemberEatMe <vitaliiseptember@gmail.com>
2020-08-28 23:37:38 +0300

    ASP.CORE start learn front

---
+++ RemoteAdmin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RemoteAdmin.Models;
+
+namespace RemoteAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+       
+    }
+}
