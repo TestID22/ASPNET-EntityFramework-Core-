ccd706645b975b4ab7b4426cfcd61b5e29d0581e
septemberEatMe <vitaliiseptember@gmail.com>
2020-09-13 08:28:56 +0300

    recode PDB

---
+++ RemoteAdmin/Models/PasswordsDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteAdmin.Models
+{
+    public class PasswordsDBContext : DbContext
+    {
+        public DbSet<UserData> UserDatas { get; set; } 
+        public PasswordsDBContext(DbContextOptions<PasswordsDBContext> options) : base(options)
+        {
+                
+        }
+    }
+}
