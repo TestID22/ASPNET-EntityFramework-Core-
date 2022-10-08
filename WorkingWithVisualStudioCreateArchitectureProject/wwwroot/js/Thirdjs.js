95cefa1a8b6298dd3e9db60c0d963dc2e79ccfda
vitaliiSeptemner <vitaliiseptember@gmail.com>
2022-10-08 09:35:23 +0300

    cleaning up

--- WorkingWithVisualStudioCreateArchitectureProject/wwwroot/js/Thirdjs.js
+++
@@ -1,5 +0,0 @@
-﻿document.addEventListener("DomContentLoadede", function () {
-    var element = document.createElement("p");
-    element.textContent("Это JS элемент");
-    document.querySelector("body").appendChild(element);
-});
--- WorkingWithVisualStudioCreateArchitectureProject/wwwroot/js/fourth.js
+++
@@ -1,5 +0,0 @@
-﻿document.addEventListener("DomContentLoaded", function () {
-    var element = document.createElement("p");
-    element.textContent("4ый элеметн JS");
-    document.querySelector("body").appendChild(element);
-});
